2336d195ea4f574c5b4a15ca4802f0b560312baa
IlzJokerzlI <arvin.lee@binus.ac.id>
2020-06-04 10:43:58 +0700

    Reset Master Branch

--- src/Main.cpp
+++ src/Main.cpp
@@ -6,17 +6,7 @@ using std::cout;
 using std::endl;
 using std::string;
 
-#include "Header/AVLTree.hpp"
-
 int main() {
-    AVLTree avl = AVLTree();
-
-    avl.insert(10);
-    avl.insert(5);
-    avl.insert(7);
-
-    cout << avl.getRoot()->value << endl;
-    cout << avl.getRoot()->lPtr->value << endl;
-    cout << avl.getRoot()->rPtr->value << endl;
+    cout << "Compagnie Search << endl;
     return 0;
 }
